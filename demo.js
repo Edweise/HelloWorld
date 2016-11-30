1a3e610b5ee25bfa4a766279e7ddd27ccc9d56e4
Edweise <1109144034@qq.com>
2016-11-30 17:36:57 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -67,3 +67,19 @@ function get_primes(arr){
     });
     return p;
 }
+
+//sort()
+//忽略大小写按照字母顺序排序
+var arr = ['Google', 'apple', 'Microsoft'];
+arr.sort(function (s1, s2) {
+    x1 = s1.toUpperCase();
+    x2 = s2.toUpperCase();
+    if (x1 < x2) {
+        return -1;
+    }
+    if (x1 > x2) {
+        return 1;
+    }
+    return 0;
+}); // ['apple', 'Google', 'Microsoft']
+
