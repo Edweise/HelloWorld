7a9fc71374f6a109b3fc85f2a5d29bda85597916
Edweise <1109144034@qq.com>
2016-11-30 15:22:00 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -32,3 +32,11 @@ var r= arr.filter(function(x){
    return x % 2 !== 0;
 });
 r;//[1,5]
+
+//把一个array中的空字符串删掉
+var arr = ['A', '', 'B', null, undefined, 'C', '  '];
+var r = arr.filter(function (s) {
+    return s && s.trim(); // 注意：IE9以下的版本没有trim()方法
+});
+arr; // ['A', 'B', 'C']
+
